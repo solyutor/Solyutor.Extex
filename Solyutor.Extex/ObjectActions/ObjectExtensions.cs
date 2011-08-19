b6d40d66db184e436eb8b443b84fd2bf1f18e4d7
solyutor <yurysold@googlemail.com>
2011-08-19 10:18:29 +0400

    Added object extensions

---
+++ Solyutor.Extex/ObjectActions/ObjectExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Solyutor.Extex.ObjectActions
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsNull(this object self)
+        {
+            return ReferenceEquals(self, null);
+        }
+
+        public static bool IsNotNull(this object self)
+        {
+            return ReferenceEquals(self, null);
+        }
+
+        public static void DoIfNull(this object self, Action action)
+        {
+            if (self.IsNotNull()) return;
+            action();
+        }
+
+        public static void DoIfNotNull(this object self, Action action)
+        {
+            if (self.IsNull()) return;
+            action();
+        }
+
+        public static void DoIfNotNull<TObject>(this TObject self, Action<TObject> action)
+        {
+            if (self.IsNull()) return;
+            action(self);
+        }
+    }
+}
