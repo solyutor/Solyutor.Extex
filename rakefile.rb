3976471354db0495665d04270a8415c844265343
Jury Soldatenkov <yurysold@googlemail.com>
2011-10-05 11:49:11 +0400

    Added nunit task to build file

--- rakefile.rb
+++ rakefile.rb
@@ -2,3 +2,7 @@ task :default do
   msbuild = "C:/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe"
   sh "#{msbuild} SolYUtor.Extex.sln"
 end
+
+task :unittests do
+  sh "Packages/NUnit.2.5.10.11092/tools/nunit-console.exe  Solyutor.Extex.Tests/bin/Debug/Solyutor.Extex.Tests.dll"
+end
